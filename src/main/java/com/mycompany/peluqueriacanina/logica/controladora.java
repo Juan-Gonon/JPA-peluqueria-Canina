4cbb7e3c77b9d15997063862de123e88127c21d3
Juan Gonon <juanfegonon27@gmail.com>
2025-01-11 16:55:10 +0000

    conexion controladoras

--- src/main/java/com/mycompany/peluqueriacanina/logica/controladora.java
+++ src/main/java/com/mycompany/peluqueriacanina/logica/controladora.java
@@ -1,5 +1,10 @@
 package com.mycompany.peluqueriacanina.logica;
 
+import com.mycompany.peluqueriacanina.persistencia.controladoraPersistencia;
+
 public class controladora {
+    
+    controladoraPersistencia controlPersis = new controladoraPersistencia();
+    
     
 }
--- src/main/java/com/mycompany/peluqueriacanina/persistencia/controladoraPersistencia.java
+++ src/main/java/com/mycompany/peluqueriacanina/persistencia/controladoraPersistencia.java
@@ -2,4 +2,7 @@ package com.mycompany.peluqueriacanina.persistencia;
 
 public class controladoraPersistencia {
     
+      DuenioJpaController duenioJpa = new DuenioJpaController();
+      MascotaJpaController mascotaJpa = new MascotaJpaController();
+    
 }
